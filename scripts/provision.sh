2d8fd1b218b30925ff345290b62de6a9f3020e9e
yesquines <yago.fut@gmail.com>
2022-05-31 11:34:07 -0300

    fix: ajuste de provisionamento

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Instalando Pacotes para o Loadbalancer e NFS
-apt-get install -y vim nfs-kernel-server nfs-common -qq &> /dev/null && \
-				echo "OK - Instalando Pacotes - NFS"
+apt-get update && apt-get install -y vim nfs-kernel-server nfs-common -qq &> /dev/null && echo "OK - Instalando Pacotes - NFS"
 
 # Configurando Pasta do NFS
 mkdir -p /srv/app && echo "OK - Configurada Pastas do NFS"
 
 # Configurando Exports do NFS 
-if ! grep '/srv/app' /etc/exports then;
-  echo '/srv/app 172.27.2.0/255.255.0.0(rw,no_root_squash,subtree_check)' >> /etc/exports && /
-  systemctl restart nfs-kernel-server && echo 'OK - Exports NFS configurado'
+if ! grep '/srv/app' /etc/exports; then
+  echo '/srv/app 172.27.2.0/255.255.0.0(rw,no_root_squash,subtree_check)' >> /etc/exports && systemctl restart nfs-kernel-server && echo 'OK - Exports NFS configurado'
 fi
 
